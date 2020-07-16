89966375f44f20a99a905661cc2a5f724339b17c
tejaspatilcz4873 <68076662+tejaspatilcz4873@users.noreply.github.com>
2020-07-16 23:55:36 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,3 @@
+//One way to ask a user for data is by using the JavaScript prompt command. To try out the prompt command, open the JavaScript console and type the following:
+
+prompt("What is your name?");
